05328ea3405849d8ac6611ab401fd988e4242ec2
Martijn Maas <martijn.maas@di.huc.knaw.nl>
2020-04-24 15:41:14 +0200

    Use a proper seed

--- test_package.py
+++ test_package.py
@@ -1,5 +1,5 @@
 import imageio
-import txt_img_aug as tia
+from txt_img_aug import TxtImgAug
 from pathlib import Path
 import numpy.random as random
 import numpy as np
@@ -15,24 +15,26 @@ for arg in sys.argv:
     if str.startswith(name_val[0], "image"):
         imagePath = name_val[1]
     elif str.startswith(name_val[0], "seed"):
-        seed = float(name_val[1])
+        seed = int(name_val[1])
 
 image = imageio.imread(imagePath)
 
 print("using image: {}".format(imagePath))
 
+tia = TxtImgAug(seed)
+
 augs = (tia.add_gausian_noise, tia.change_gamma_contrast, tia.shear, tia.blur)
 
 perm = list(permutations(augs)) + list(permutations(augs,3)) + list(permutations(augs,2)) + list(permutations(augs,1))
 
-nr=0
+no=0
 for permutation in perm:
     augImage = image
     for augmentation in permutation:
         if seed is not None:
-            augImage = augmentation(augImage, seed)
+            augImage = augmentation(augImage)
         else:
             augImage = augmentation(augImage)
 
-    imageio.imwrite("output/testimage{}.png".format(nr), augImage)
-    nr+=1
+    imageio.imwrite("output/testimage{}.png".format(no), augImage)
+    no+=1
--- txt_img_aug.py
+++ txt_img_aug.py
@@ -3,33 +3,37 @@ import imgaug.augmenters as iaa
 import numpy as np
 import numpy.random as random
 
+class TxtImgAug:
 
-def add_gausian_noise(image: np.ndarray, seed = random.random()) -> np.ndarray:
-    loc = seed * 10
-    scale = seed * 20
-    noise = iaa.AdditiveGaussianNoise(loc, scale)
-    return noise.augment_image(image)
+    def __init__(self, seed:int=None):
+        random.seed(seed)
 
-def change_gamma_contrast(image: np.ndarray, seed = random.random()) -> np.ndarray:
-    gamma = seed * 4
-    gamma = gamma if gamma > 0 else gamma * -1
-    gamma = gamma * 2 if gamma > 1 else gamma / 2
-    gamma = max(2, gamma)
-    gamma = min(0.5, gamma)
+    def add_gausian_noise(self, image: np.ndarray) -> np.ndarray:
+        loc = random.random() * 10
+        scale = random.random() * 20
+        noise = iaa.AdditiveGaussianNoise(loc, scale)
+        return noise.augment_image(image)
 
-    contrast = iaa.GammaContrast(gamma=gamma)
-    return contrast.augment_image(image)
+    def change_gamma_contrast(self, image: np.ndarray) -> np.ndarray:
+        gamma = random.random() * 4
+        gamma = gamma if gamma > 0 else gamma * -1
+        gamma = gamma * 2 if gamma > 1 else gamma / 2
+        gamma = max(2, gamma)
+        gamma = min(0.5, gamma)
 
-def shear(image: np.ndarray, seed = random.normal()) -> np.ndarray:
-    shearx = seed * 0.03
-    sheary = seed * 0.05
-    shear = iaa.Affine(shear=(shearx, sheary))
-    
-    return shear.augment_image(image)
+        contrast = iaa.GammaContrast(gamma=gamma)
+        return contrast.augment_image(image)
 
-def blur(image: np.ndarray, seed = random.normal()) -> np.ndarray:
-    sigmastart = max(0, seed * 3)
-    sigmaend = min(3, max(0, seed * 20))
-    blur = iaa.GaussianBlur(sigma=(sigmastart, sigmaend))
+    def shear(self, image: np.ndarray) -> np.ndarray:
+        shearx = random.normal() * 0.03
+        sheary = random.normal() * 0.05
+        shear = iaa.Affine(shear=(shearx, sheary))
+        
+        return shear.augment_image(image)
 
-    return blur.augment_image(image)
+    def blur(self, image: np.ndarray) -> np.ndarray:
+        sigmastart = max(0, random.normal() * 3)
+        sigmaend = min(3, max(0, random.normal() * 20))
+        blur = iaa.GaussianBlur(sigma=(sigmastart, sigmaend))
+
+        return blur.augment_image(image)
