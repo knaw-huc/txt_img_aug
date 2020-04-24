ff6fd4dbf792774f07d3b47b582b42ece38eb334
Martijn Maas <martijn.maas@di.huc.knaw.nl>
2020-04-24 14:02:22 +0200

    Allow arguments for seed and image

--- test_package.py
+++ test_package.py
@@ -4,10 +4,23 @@ from pathlib import Path
 import numpy.random as random
 import numpy as np
 from itertools import permutations
+import sys
 
 Path("output").mkdir(parents=True, exist_ok=True)
 
-image = imageio.imread("testimage.png")
+image = None
+seed = None
+for arg in sys.argv:
+    name_val = arg.split("=")
+    if str.startswith(name_val[0], "image"):
+        image = imageio.imread(name_val[1])
+    elif str.startswith(name_val[0], "seed"):
+        seed = int(name_val[1])
+
+
+if image is None:
+    print("using default image: testimage.png")
+    image = imageio.imread("testimage.png")
 
 augs = (tia.add_gausian_noise, tia.change_gamma_contrast, tia.shear, tia.blur)
 
@@ -17,7 +30,10 @@ nr=0
 for permutation in perm:
     augImage = image
     for augmentation in permutation:
-        augImage = augmentation(augImage)
+        if seed is not None:
+            augImage = augmentation(augImage, seed)
+        else:
+            augImage = augmentation(augImage)
 
     imageio.imwrite("output/testimage{}.png".format(nr), augImage)
     nr+=1
