babc4ba6eeb1a7be5cf3c35675a9c5a3ef43e8ba
Martijn Maas <martijn.maas@di.huc.knaw.nl>
2020-06-12 15:15:16 +0200

    Make project usable in other projects

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+# https://stackoverflow.com/questions/44070953/python-import-package-from-different-project/44071394#44071394
+# https://docs.python.org/3/distutils/setupscript.html
+# Install project: pip3 install -e .
+setup(name='txt_img_aug',
+      version='0.1')
